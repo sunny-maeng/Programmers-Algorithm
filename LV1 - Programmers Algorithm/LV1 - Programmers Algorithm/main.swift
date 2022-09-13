47dd514f1f4b1e70ab4fb9dc88ced74830db3ae7
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-13 11:51:40 +0900

    자릿수 더하기
    1.map을 이용해 Array[String]에 담아서 자릿수 별로 더해주기
    
    2. reduce를 이용해 Array[String]에서 클로저에서 더해주기
    
    3. %10 을 이용해 나머지를 더해주는 방식

--- LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/main.swift
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/main.swift
@@ -1,23 +1,44 @@
 /*
- 짝수와 홀수
- https://school.programmers.co.kr/learn/courses/30/lessons/12937
- 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수
- num은 int 범위의 정수입니다.
- 0은 짝수입니다.
+ 자릿수 더하기
+ https://school.programmers.co.kr/learn/courses/30/lessons/12931
+ 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+ 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
  */
 
 import Foundation
 
-func solution(_ num:Int) -> String {
-    return num % 2 == 0 ? "Even" : "Odd"
+func solution(_ n:Int) -> Int {
+    var answer:Int = 0
+    let numberArray = String(n).map{String($0)}
+    
+    for number in 0...numberArray.count-1 {
+        answer += Int(numberArray[number])!
+    }
+    
+    return answer
 }
 
-func solution2(_ num:Int) -> String {
-    if num % 2 == 0 {
-        return "Even"
-    } else {
-        return "Odd"
-    }
+
+func solution2(_ n:Int) -> Int {
+        return String(n).reduce(0, { Int($0) + Int(String($1))! })
 }
+
+func solution3(_ n:Int) -> Int
+{
+    var answer:Int = 0
+    var number = n
+
+    while number > 0 {
+        answer += number % 10
+        number /= 10
+    }
+
+    return answer
+}
+
+print(solution(11))
+print(solution2(11))
+print(solution(211))
+
 
 
