937be98194b9a6d54b9769d3e06e27909745d85e
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-13 23:21:50 +0900

    정수 내림차순으로 배치하기
    1. map 이용 array로 만들어 준 후 sorted(bt:>) 로 내림차순의 array로 배열, 그다음 Array [string]을  String체로 array에서 꺼내와서 +로 합친후 마지막에 Int64로 변형
    
    2, 3. array안 만들고 sorted로 String나열 (강제언래핑필수)

---
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/정수 내림차순으로 배치하기.swift
@@ -0,0 +1,43 @@
+/*
+ 정수 내림차순으로 배치하기
+ https://school.programmers.co.kr/learn/courses/30/lessons/12933
+ 
+ 함수 solution은 정수 n을 매개변수로 입력받습니다.
+ n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요.
+ 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+ 제한 조건
+ n은 1이상 8000000000 이하인 자연수입니다.
+ */
+import Foundation
+
+
+//오답 형변환시 옵셔널이라 꺼낼때 강제 언래핑 필요
+// 1. array에서 다시 꺼낼 때 String +로 붙여주기
+
+func solution(_ n:Int64) -> Int64 {
+    let array = String(n).map { String($0) }.sorted(by:>)
+    var answer = String()
+    
+    for i in 1..<array.count {
+        answer += array[i]
+    }
+    
+    return Int64(answer)!
+}
+
+print(solution(118372))
+
+func solution2(_ n:Int64) -> Int64 {
+    return Int64(String(String(n).sorted{ $0 > $1 }))!
+}
+
+func solution3(_ n:Int64) -> Int64 {
+    return Int64(String(String(n).sorted(by: >)))!
+}
+
+
+let a = 1236563
+print(String(a).sorted{$0 > $1}) // ["6", "6", "5", "3", "3", "2", "1"]
+print(String(String(a).sorted{$0 > $1})) // 6653321
+
