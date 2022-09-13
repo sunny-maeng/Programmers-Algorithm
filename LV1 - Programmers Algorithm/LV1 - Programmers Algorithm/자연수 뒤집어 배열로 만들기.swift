17c1353fd3ede364dea95d8756cf3cbafac7610d
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-13 13:56:29 +0900

    자연수 뒤집어 배열로 만들기
    1. map, reversed 사용

---
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/자연수 뒤집어 배열로 만들기.swift
@@ -0,0 +1,17 @@
+/*
+ 자연수 뒤집어 배열로 만들기
+ https://school.programmers.co.kr/learn/courses/30/lessons/12932
+ 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요.
+ 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+ 제한 조건
+ n은 10,000,000,000이하인 자연수입니다.
+ */
+
+import Foundation
+
+func solution(_ n:Int64) -> [Int] {
+    return String(n).map{ Int(String($0))! }.reversed()
+}
+
+print(solution(12345)) // [5,4,3,2,1]
