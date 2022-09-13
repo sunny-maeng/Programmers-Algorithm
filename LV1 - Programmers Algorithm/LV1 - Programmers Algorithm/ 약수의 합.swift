b7a3aa862d125138bdea3bca0e204dce60683dd8
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-09-13 13:00:39 +0900

    약수의 합
    n을 입력받아 n의 약수를 모두 더한 값
    
    오답: 0일때 생각해줘야한다.
    
    1. n=0일때 if 처리 후, 1~n까지 n을 나눈 후 나머지가 0인 값을 더해주기
    
    2. 1~n까지 Array만들고, filter { } 로 나머지가 0인값을 골라준 후 reduce를 이용해 더해주기

---
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/ 약수의 합.swift
@@ -0,0 +1,35 @@
+/*
+ 약수의 합
+ https://school.programmers.co.kr/learn/courses/30/lessons/12928
+정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+ 제한: n은 0 이상 3000이하인 정수입니다.
+ */
+
+//오답 :  0일 때를 생각하지 못해서 0일때 for 구문이 돌지 못해서 오류났었음
+// if n == 0 일 때 추가해줌. 답지보니까 guard 처리가 나을 것 같음.
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var answer: Int = 0
+    
+    if n == 0 {
+        return 0
+    }
+    
+    for number in 1...n {
+        if n % number == 0 {
+            answer += number
+        }
+    }
+    
+    return answer
+}
+
+
+func solution2(_ n:Int) -> Int {
+    guard n != 0 else {
+        return 0
+    }
+    return Array(1...n).filter{n % $0 == 0}.reduce(0, +)
+}
