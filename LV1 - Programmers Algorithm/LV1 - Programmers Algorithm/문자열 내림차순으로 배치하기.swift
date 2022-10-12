aa1f7a9e268599e4429d4fc03b949ea3e9361358
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-12 21:37:57 +0900

    문자열 내림차순으로 배치하기

---
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/문자열 내림차순으로 배치하기.swift
@@ -0,0 +1,24 @@
+/*
+ 문자열 내림차순으로 배치하기
+ 문제 설명
+ 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+ s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+ 제한 사항
+ str은 길이 1 이상인 문자열입니다.
+ 
+ 입출력 예
+ s             return
+ "Zbcdefg"    "gfedcbZ"
+ */
+
+import Foundation
+
+
+func solution(_ s:String) -> String {
+    s.map { String($0) }.sorted(by: >).joined()
+}
+
+func solution2(_ s:String) -> String {
+    return String(s.sorted { $0 > $1 })
+}
