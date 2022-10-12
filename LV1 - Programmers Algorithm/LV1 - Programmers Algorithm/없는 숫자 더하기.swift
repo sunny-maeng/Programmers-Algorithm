6a01374e1889caa54b4d43ea369bd38b106834f7
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2022-10-12 17:08:41 +0900

    없는 숫자 더하기

---
+++ LV1 - Programmers Algorithm/LV1 - Programmers Algorithm/없는 숫자 더하기.swift
@@ -0,0 +1,29 @@
+/*
+ 없는 숫자 더하기
+ 
+ 문제 설명
+ 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+ 
+ 제한사항
+ 1 ≤ numbers의 길이 ≤ 9
+ 0 ≤ numbers의 모든 원소 ≤ 9
+ numbers의 모든 원소는 서로 다릅니다.
+ 
+ 입출력 예
+ numbers              result
+ [1,2,3,4,6,7,8,0]       14
+ [5,8,4,0,6,7,9]         6
+ */
+
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> Int {
+    let arr = Array(1...9)
+    return arr.reduce(0,+) - numbers.reduce(0,+)
+}
+
+func solution2(_ numbers: [Int]) -> Int {
+    return (0...9).filter { !numbers.contains($0) }.reduce(0, +)
+}
